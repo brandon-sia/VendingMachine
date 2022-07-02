e635bbcdf514d960543c11d0bca9b00f017f427f
Brandon Sia <brandonsia@hotmail.com>
2022-07-02 12:12:39 +1000

    Add project files.

---
+++ VendingMachine/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace VendingMachine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VendingMachine vendingMachine = new AnimalFeedVendingMachine();
+            Console.WriteLine(vendingMachine.Dispense(2.00M));
+        }
+    }
+
+    class VendingMachine
+    {
+        public virtual string Item { get; }
+        protected virtual bool CheckAmount(decimal money)
+        {
+            return false;
+        }
+
+        public string Dispense(decimal money)
+        {
+            if (CheckAmount(money)) return Item;
+            else return "Please enter the right amount";
+        }
+    }
+
+    class AnimalFeedVendingMachine : VendingMachine
+    {
+        public override string Item
+        {
+            get { return "a handful of animal feed"; }
+        }
+        protected override bool CheckAmount(decimal money)
+        {
+            return money >= 1.25M;
+        }
+    }
+}
